34cc6da17936061e90c43137c6f8961d96b5d408
Fl023 <115723377+Fl023@users.noreply.github.com>
2024-04-22 23:38:39 +0200

    added script to run on cluster

--- Ex1/script.sh
+++ Ex1/script.sh
@@ -2,4 +2,4 @@
 
 file_path="hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json"
 
-python run.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r "$file_path"
+python run.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop "$file_path"

1897dc5c05c272629e819a0f321c3af22c15178c
Fl023 <115723377+Fl023@users.noreply.github.com>
2024-04-22 09:43:09 +0200

    changed output

--- Ex1/run.py
+++ Ex1/run.py
@@ -1,9 +1,11 @@
 from main import ChiSquaredCalculator
 from sub import CategoryCounter
 from collections import defaultdict
+import time
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     job1 = CategoryCounter()
     job2 = ChiSquaredCalculator()
     job2.FILES = ["./stopwords.txt", "./category_counts.txt"]
@@ -19,14 +21,23 @@ if __name__ == "__main__":
             for key in category_counts:
                 file.write(str(key) + " " + str(category_counts[key]) + "\n")
 
+    job1_runtime = time.time() - start_time
+
+    output_file = "output.txt"
     with job2.make_runner() as runner:
         runner.run()
         all_words = []
         my_dict = defaultdict(lambda: "")
-        for key, value in job2.parse_output(runner.cat_output()):
-            term, chi = value
-            all_words.append(term)
-            my_dict[key] += " " + term + ":" + str(chi)
-        for key in my_dict:
-            print("<"+key+">"+my_dict[key])
-        print(*sorted(set(all_words)))
+        with open(output_file, "w") as output:
+            for key, value in job2.parse_output(runner.cat_output()):
+                term, chi = value
+                all_words.append(term)
+                my_dict[key] += " " + term + ":" + str(chi)
+            for key in my_dict:
+                output.write("<" + key + ">" + my_dict[key] + "\n")
+            output.write(" ".join(sorted(set(all_words))) + "\n")
+
+    job2_runtime = time.time() - (start_time + job1_runtime)
+
+    print("Job 1 Runtime:", job1_runtime, "seconds")
+    print("Job 2 Runtime:", job2_runtime, "seconds")

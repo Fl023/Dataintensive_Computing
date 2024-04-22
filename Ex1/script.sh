f0bb124f39e32e4cd8e1c1e44c73fc03d8c350a6
Fl023 <115723377+Fl023@users.noreply.github.com>
2024-04-22 23:56:54 +0200

    moving

--- Ex1/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-file_path="hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json"
-
-python run.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop "$file_path"
